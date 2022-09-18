51fdd9b0e1b2d427f7a8ad5c50b094c64d3a8b9a
Chinna Rayapati <joseph.chinna533@gmail.com>
2022-09-18 12:40:54 +0000

    edited app.js file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('Hello World from KarunaChinna Docker Tutorials!!');
 })
 
 app.listen(port, host);
